72b5b606f39299f7d311aa1c8ffa40b4248350cf
hacunningham67 <74032781+holly683@users.noreply.github.com>
2022-12-05 17:14:23 +0000

    updated newUser so that it makes a dictionary

--- Management.py
+++ Management.py
@@ -2,7 +2,19 @@ import db
 from User import *
 class Management():
   def newUser(self,username,password,firstName,lastName,birthday,gender,romanticPreference):
-    db[username] = [password,firstName,lastName,birthday,gender,romanticPreference]
+    #adds a user to the dictionary and creates a user object that is returned
+    dic = {
+      'password': password, 
+      'firstName': firstName,
+      'lastname': lastName,
+      'birthday': birthday,
+      'gender': gender,
+      'romanticPreference': romanticPreference
+    }
+    db[username] = dic
     return User(username,password,firstName,lastName,birthday,gender,romanticPreference)
+  def findMatches(self,userToMatch):
+    #looks through the dictionary to find the users that most closely match with the user looking for matches. returns a dictionary of the 5 top matches ordered by percentage
+  
     
   
