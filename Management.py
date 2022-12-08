82c0009b70978bf6ae1b24f538e46f7110227eba
dilxnaz <119353208+dilxnaz@users.noreply.github.com>
2022-12-08 22:21:05 +0000

    fixed some syntax errors

--- Management.py
+++ Management.py
@@ -29,17 +29,17 @@ class ManagementSystem():
   def getMBTIChart(self):
     return self.__mbtiChart
   def getTraitCompatibilityPercentage(user1,user2):
-  pT1 = 0
-  for x in user1.getTraitsWanted():
-    if x in user2.getTraits():
-      pT1 = pT1 + 1
-  pT1 = pT1/len(utW)
-  pT2 = 0 
-  for x in user2.getTraitsWanted():
-    if x in user1.getTraits():
-      pT2 = pT2 + 1
-  pT2 = pT2/len(otW)
-  return (pT1 + pT2)*50
+    pT1 = 0
+    for x in user1.getTraitsWanted():
+      if x in user2.getTraits():
+        pT1 = pT1 + 1
+    pT1 = pT1/len(utW)
+    pT2 = 0 
+    for x in user2.getTraitsWanted():
+      if x in user1.getTraits():
+        pT2 = pT2 + 1
+    pT2 = pT2/len(otW)
+    return (pT1 + pT2)*50
   def compareLists(l1,l2):
     longer = max(l1,l2)
     shorter = min(l1,l2)
@@ -65,10 +65,11 @@ class ManagementSystem():
     #returns the compatibility percentage (from 0-100) between two mbti types
     #this can then be used with other compatibility percentages to calculate an overall average compatibility
     return int(self.getMBTIChart()[self.getMBTIListIndex(user1.getMBTI())][self.getMBTIListIndex(user2.getMBTI())])
+    
   def getMatches(self,mainUser):
     filteredUserL = list(filter(lambda x: (x.getGender() in mainUser.getRomanticPreference() and mainUser.getGender() in x.getRomanticPreference()), self.getUserList()))
     possibleMatches = list(filter(lambda x: (x.getUsername()!=mainUser.getUsername()),filteredUserL))
-bestMatches = []
+    bestMatches = []
     for match in possibleMatches:
       percentages = []
       percentages.append(getMbtiCompatibilityPercentage(mainUser, match))
